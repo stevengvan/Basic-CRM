3ed6cc651028bd2abd82b4efb152486557117e98
stevengvan <vansteven17@gmail.com>
2023-07-31 19:27:05 -0700

    revised api URL

--- client/src/apiURL.js
+++ client/src/apiURL.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "https://basic-crm-server.adaptable.app";
+const baseURL = "https://basic-crm-server.adaptable.app/customers";
 // const baseURL = "http://localhost:3000/customers";
 
 export default {
